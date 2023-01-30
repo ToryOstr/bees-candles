909edb542baeba9dbff24fa12566b21f37b41562
Viktoriia Ostrishko <vik.ostrishko@gmail.com>
2023-01-30 10:38:12 +0200

    add footer

--- constants/root.js
+++ constants/root.js
@@ -1,3 +1,5 @@
 const ROOT_CATALOG = document.querySelector(".catalog");
 const ROOT_BASKET = document.querySelector(".basket-card");
-const PRODUCT_VEIW = document.querySelector(".product-big-card");
+const PRODUCT_VEIW = document.querySelector(".product-card-body");
+
+const PRODUCT_REVEIW = [];
