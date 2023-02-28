cc3e40f988ba6c56baeaae2bde76711b9873700f
Viktoriia Ostrishko <vik.ostrishko@gmail.com>
2023-03-01 01:27:26 +0200

    finish basket card. Stort add order form

--- components/Basket-card/Basket-card.js
+++ components/Basket-card/Basket-card.js
@@ -7,13 +7,25 @@ let mainBasketCard = document.querySelector(".basket-opened");
 let thead = mainBasketCard.querySelector("thead");
 let tbody = mainBasketCard.querySelector("tbody");
 let tfoot = mainBasketCard.querySelector("tfoot");
+let amountValue = 0;
+let countingProducts = 0;
+
+function calcOrderAmount() {
+  mainBasket.forEach((el) => {
+    amountValue += el["sum"];
+    countingProducts += el["quantity products"];
+  });
+}
 
 function generatedBasket(array) {
+  calcOrderAmount();
   if (mainBasket.length === 0) {
     thead.innerHTML = `
             <tr class="t-head">
               <th colspan="9">Ви ще не обрали жодного товару</th>
             </tr>`;
+    tbody.innerHTML = "";
+    tfoot.innerHTML = "";
   } else {
     thead.innerHTML = `
             <tr class="t-head">
@@ -38,10 +50,10 @@ function generatedBasket(array) {
               <td class="saturation">${elem["color saturation"]}</td>
               <td class="quantity">${elem["quantity products"]}</td>
               <td class="packaging">${elem["packaging"]}</td>
-              <td class="price">${elem["product price"]}</td>
+              <td class="price">${elem["product price"]}грн</td>
               <td class="summ">${
                 elem["product price"] * elem["quantity products"]
-              }</td>
+              }грн</td>
               <td>
                 <button class="remove-product">
                   <img
@@ -55,11 +67,43 @@ function generatedBasket(array) {
       })
       .join("");
     tfoot.innerHTML = `
-            <th scope="row" colspan="7">Сума замовлення:</th>
-            <td class="quantity"></td>
-            <td class="amount-value"></td>
+    <tr>
+      <th scope="row" colspan="9" class="order-amount">Сума замовлення: ${amountValue}грн</th>
+    </tr>
+    <tr>
+      <th scope="row" colspan="9" class="order-amount">Кількість товару в кошику: ${countingProducts}</th>
+    </tr>
+
     `;
+    mainBasketCard.insertAdjacentHTML(
+      "beforeend",
+      `
+      <button class="order-btn">Перейти до оформлення замовлення</button>`
+    );
   }
+  let removeProductBtns = document.querySelectorAll(".remove-product");
+  removeProductBtns.forEach(function (btn) {
+    btn.addEventListener("click", removeProduct);
+  });
+  let orderBtn = document.querySelector(".order-btn");
+  orderBtn.addEventListener("click", function () {
+    window.open("order-form.html", "_self");
+  });
 }
 generatedBasket(mainBasket);
 recountProductsCounter();
+
+function removeProduct() {
+  let elementIndex =
+    Number(this.closest("tr").querySelector(".number").innerText) - 1;
+  mainBasket.splice(elementIndex, 1);
+  localStorage.setItem("basket", JSON.stringify(mainBasket));
+  mainBasket =
+    JSON.parse(localStorage.getItem("basket")) === null
+      ? []
+      : JSON.parse(localStorage.getItem("basket"));
+  amountValue = 0;
+  countingProducts = 0;
+  recountProductsCounter();
+  generatedBasket(mainBasket);
+}
